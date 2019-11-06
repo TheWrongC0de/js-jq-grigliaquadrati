1a49e2006158c9110a16846465c1774058b27e21
Simone <simoneugazio1993@gmail.com>
2019-11-06 16:44:48 +0100

    push 3

--- js/main.js
+++ js/main.js
@@ -3,20 +3,14 @@
 //gli altri diventano verdi (tutti i rimanennti)
 //Opzionale: Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati //scoperti
 $(document).ready(function () {
-
-    var punteggio = 0;
-
+    var punti = 0;
 $("td").click(function (){
     $(this).css("background-color" , "red")
 })
-
-    punteggio ++;
-    console.log(punteggio)
+    punti ++;
+    console.log(punti)
 });
 
 $("td").click(function (){
     $(this).css("background-color" , "green")
-})
-
-});
 });
